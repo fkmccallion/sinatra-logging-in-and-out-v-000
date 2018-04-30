9fe9d996fdbb8d9afb6b456d3b7073fe2d1e0967
Frank McCallion <fkmccallion@gmail.com>
2018-04-30 13:15:10 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,7 +1,6 @@
 class Helpers
   def self.current_user(session_hash)
-    @user = User.find(session_hash[:user_id])
-
+    User.find(session_hash[:user_id])
   end
 
   def self.is_logged_in?(session_hash)
