0591ae6fa23480b107ab139557b673c9516a7248
Frank McCallion <fkmccallion@gmail.com>
2018-04-30 13:14:42 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -5,11 +5,10 @@ class Helpers
   end
 
   def self.is_logged_in?(session_hash)
-    #if session_hash[:user_id] == nil
-    #  false
-    #else
-    #  true
-    #end
-    !session[:user_id].nil?
+    if session_hash[:user_id] == nil
+      false
+    else
+      true
+    end
   end
 end
